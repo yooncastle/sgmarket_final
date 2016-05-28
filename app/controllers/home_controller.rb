6d36604f6fe01819dcfc6040612f04b21f9037b2
yooncastle <yooncastle@gmail.com>
2016-05-28 04:47:59 +0000

    Thrird

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @posts = Post.all
   end
-
+  
   def write
+    @new_post = Post.new
+    @new_post.title = params[:title]
+    @new_post.content = params[:content]
+    @new_post.save
   end
+  
+ 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
-  get 'home/write'
+  match ":controller(/:action(/:id))", via: [:get, :post]
+  get 'home/index' 
+  post 'home/write'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
