28e09e235b720b2807a12e321c8f2c956d9312d0
bokyungkang <ladybk0729@gmail.com>
2016-07-07 13:17:35 +0000

    thumbnailv.1

--- app/uploaders/mktimage_uploader.rb
+++ app/uploaders/mktimage_uploader.rb
@@ -33,9 +33,12 @@ class MktimageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-   process :resize_to_fit => [350, 350]
+   process :resize_to_fit => [200, 200]
   end
 
+  version :medium do
+   process :resize_to_fit => [450, 450]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
